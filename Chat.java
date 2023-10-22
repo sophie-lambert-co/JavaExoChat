c640737d1caf50275095fd5311c0dced19695d8d
sophie-lambert-co <lemaildesophielambert@gmail.com>
2023-10-22 22:36:01 +0200

    first commit

---
+++ Chat.java
@@ -0,0 +1,11 @@
+package JavaExoChat;
+
+public class Chat {
+    public void miauler (){
+        System.out.println("Miiiiaouuuuuu");
+    }
+    public void aboyer (){
+        System.out.println("Wooof!");
+    }
+}
+
---
+++ Main.java
@@ -0,0 +1,14 @@
+package JavaExoChat;
+
+public class Main {
+    public static void main(String[] args){
+        // Instanciation de la classe Chat
+        Chat monChat = new Chat();
+
+        // Appel de la methode Miauler
+        monChat.miauler(); // Affiche "Miiiiaouuuuuu"
+        // Appel de la methode Aboyer
+        monChat.aboyer();
+    }
+}
+
